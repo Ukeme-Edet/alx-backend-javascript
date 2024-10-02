75dfdea1c06be17139976437fb740a5259d02fb2
Ukeme Edet <ukemeedet2207@gmail.com>
2024-10-02 18:25:59 +0100

    Add getFullResponseFromAPI function using Promise

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,5 @@
+function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => (success
+    ? resolve({ status: 200, body: 'Success' })
+    : reject(new Error('The fake API is not working currently'))));
+}
