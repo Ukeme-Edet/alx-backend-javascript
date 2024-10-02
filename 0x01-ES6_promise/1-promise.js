0938ad275d43db890855dfcd0a9f4a0ce05c49c6
Ukeme Edet <ukemeedet2207@gmail.com>
2024-10-02 18:27:48 +0100

    Refactor getResponseFromAPI and getFullResponseFromAPI functions to use ES6 module syntax

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,4 +1,4 @@
-function getFullResponseFromAPI(success) {
+export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => (success
     ? resolve({ status: 200, body: 'Success' })
     : reject(new Error('The fake API is not working currently'))));
