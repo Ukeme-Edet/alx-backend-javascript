7cb8d89c5532d4cda783872fadef855d1822845c
Ukeme Edet <ukemeedet2207@gmail.com>
2024-10-02 19:38:45 +0100

    Refactor handleProfileSignup function to use async/await syntax and add error handling for signup system offline

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((values) => console.log(values[0].body, values[1].firstName, values[1].lastName))
-    .catch(() => console.log('Signup system offline'));
+export default async function handleProfileSignup() {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    return console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+  } catch (err) {
+    return console.log('Signup system offline');
+  }
 }
