5425f0faa9fc92cd9188d4a32485fc120aa9914e
Ukeme Edet <ukemeedet2207@gmail.com>
2024-10-02 19:32:58 +0100

    Refactor handleProfileSignup function to remove unnecessary return statement

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
+  Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
       console.log(
         values[0].body,
