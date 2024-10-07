c2f3f35fa5d7a4749a1390337f506b0e9a5c65c6
Ukeme Edet <ukemeedet2207@gmail.com>
2024-10-07 19:50:17 +0100

    Refactor queryAPI function to handle API endpoint load tracking

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,13 +2,9 @@ const weakMap = new WeakMap();
 function queryAPI(endpoint) {
   if (weakMap.has(endpoint)) {
     const count = weakMap.get(endpoint);
-    if (count >= 5) {
-      throw new Error('Endpoint load is high');
-    }
     weakMap.set(endpoint, count + 1);
-  } else {
-    weakMap.set(endpoint, 1);
-  }
+    if (count >= 5) throw new Error('Endpoint load is high');
+  } else weakMap.set(endpoint, 1);
 }
 
 export { weakMap, queryAPI };
