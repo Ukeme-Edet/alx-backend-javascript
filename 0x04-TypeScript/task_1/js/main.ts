866c0ff7464325edd2fe33746c26884b7aaf18b5
Ukeme Edet <ukemeedet2207@gmail.com>
2024-10-09 19:49:04 +0100

    Add printTeacher function to print teacher's name initials

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,3 +10,14 @@ export interface Teacher {
 export interface Directors extends Teacher {
 	numberOfReports: number;
 }
+
+export interface printTeacherFunction {
+	(firstName: string, lastName: string): string;
+}
+
+export const printTeacher: printTeacherFunction = (
+	firstName: string,
+	lastName: string
+): string => {
+	return `${firstName.charAt(0)}. ${lastName}`;
+};
