67656a5c15a53822853f24d94d2c3d7df86714f9
Ukeme Edet <ukemeedet2207@gmail.com>
2024-10-09 20:02:19 +0100

    Add function to teach a class based on the subject

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -41,3 +41,8 @@ export function executeWork(employee: Director | Teacher): string {
 	if (isDirector(employee)) return employee.workDirectorTasks();
 	return employee.workTeacherTasks();
 }
+type Subjects = "Math" | "History";
+export function teachClass(todayClass: Subjects): string {
+	if (todayClass === "Math") return "Teaching Math";
+	return "Teaching History";
+}
