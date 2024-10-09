ceee1433a037bb3a1915ea30eb8e35d2d32a7021
Ukeme Edet <ukemeedet2207@gmail.com>
2024-10-09 20:01:26 +0100

    Add functions to check if an employee is a director and execute their work tasks

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -34,3 +34,10 @@ export const createEmployee = (salary: number | string): Director | Teacher => {
 	if (typeof salary === "number" && salary < 500) return new Teacher();
 	return new Director();
 };
+export function isDirector(employee: Director | Teacher): employee is Director {
+	return (employee as Director).workDirectorTasks !== undefined;
+}
+export function executeWork(employee: Director | Teacher): string {
+	if (isDirector(employee)) return employee.workDirectorTasks();
+	return employee.workTeacherTasks();
+}
