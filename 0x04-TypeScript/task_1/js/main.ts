7228d2774a98767056b9119fa8e77196a3fb04c7
Ukeme Edet <ukemeedet2207@gmail.com>
2024-10-09 19:53:58 +0100

    Add StudentClass and interfaces for student information

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -21,3 +21,28 @@ export const printTeacher: printTeacherFunction = (
 ): string => {
 	return `${firstName.charAt(0)}. ${lastName}`;
 };
+export interface StudentClassConstructor {
+	new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+export interface StudentClassInterface {
+	firstName: string;
+	lastName: string;
+	workOnHomework(): string;
+	displayName(): string;
+}
+
+export class StudentClass implements StudentClassInterface {
+	firstName: string;
+	lastName: string;
+	constructor(firstName: string, lastName: string) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	workOnHomework(): string {
+		return "Currently working";
+	}
+	displayName(): string {
+		return this.firstName;
+	}
+}
