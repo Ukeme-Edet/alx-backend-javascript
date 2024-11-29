fa9ef6d36f2599e0d1cc6e5e3c85f9708d9c7d96
Ukeme Edet <ukemeedet2207@gmail.com>
2024-11-29 22:42:47 +0100

    Refactor stdin.js to use 'line' event for input handling and improve welcome message

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,11 +2,14 @@ const rl = require("readline").createInterface({
     input: process.stdin,
     output: process.stdout,
 });
+console.log("Welcome to Holberton School, what is your name?");
+
+rl.on("line", (input) => {
+    console.log(`Your name is: ${input}`);
+    rl.close();
+});
 
 rl.on("close", () => {
     console.log("This important software is now closing");
     process.exit(0);
-});
-rl.question("Welcome to Holberton School, what is your name?\n", (name) => {
-    console.log(`Your name is: ${name}`);
 });
