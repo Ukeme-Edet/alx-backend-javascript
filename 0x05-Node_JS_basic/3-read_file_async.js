8bb3f068047885dcebfbc30dd0df477ac981e598
Ukeme Edet <ukemeedet2207@gmail.com>
2024-11-29 23:22:45 +0100

    Add countStudents function in 3-read_file_async.js for asynchronous student data processing

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,32 @@
+const fs = require("fs").promises;
+
+async function countStudents(pth) {
+    return await fs
+        .readFile(pth, "utf8")
+        .then((data) => {
+            const students = data
+                .trim()
+                .split("\n")
+                .slice(1)
+                .map((line) => line.split(","))
+                .filter((student) => student.length === 4);
+            console.log(`Number of students: ${students.length}`);
+            const fields = students.reduce((acc, student) => {
+                const field = student[3];
+                if (!acc[field]) acc[field] = [];
+                acc[field].push(student[0]);
+                return acc;
+            }, {});
+            for (const [field, names] of Object.entries(fields)) {
+                console.log(
+                    `Number of students in ${field}: ${
+                        names.length
+                    }. List: ${names.join(", ")}`
+                );
+            }
+        })
+        .catch(() => {
+            throw new Error("Cannot load the database");
+        });
+}
+module.exports = countStudents;
