66df23d57e6cc816f7064670d0b9b3d977c1f06e
Ukeme Edet <ukemeedet2207@gmail.com>
2024-12-01 00:36:39 +0100

    Update unit tests for calculateNumber to reflect rounding behavior

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,17 +1,16 @@
-const { describe, it } = require("mocha");
 const assert = require("assert");
-const { calculateNumber } = require("./0-calcul");
-describe("add", () => {
-    it("should add two integers", () => {
+const calculateNumber = require("./0-calcul");
+describe("calculateNumber", () => {
+    it("should add two rounded integers", () => {
         assert.strictEqual(calculateNumber(1, 3), 4);
     });
-    it("should add two floats", () => {
+    it("should add a rounded integer and a rounded float", () => {
         assert.strictEqual(calculateNumber(1, 3.7), 5);
     });
-    it("should add an integer and a float", () => {
+    it("should add two rounded floats", () => {
         assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     });
-    it("should add two negative integers", () => {
+    it("should add two rounded floats", () => {
         assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
 });
