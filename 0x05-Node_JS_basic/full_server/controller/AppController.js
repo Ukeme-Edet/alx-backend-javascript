1f59fd7966b99da5af034e39651a31a14db223cc
Ukeme Edet <ukemeedet2207@gmail.com>
2024-11-30 15:47:08 +0100

    Add AppController and StudentsController for handling HTTP requests

---
+++ 0x05-Node_JS_basic/full_server/controller/AppController.js
@@ -0,0 +1,6 @@
+class AppController {
+  static async getHomePage(req, res) {
+    res.send('Hello Holberton School!');
+  }
+}
+export default AppController;
---
+++ 0x05-Node_JS_basic/full_server/controller/StudentController.js
@@ -0,0 +1,37 @@
+import readDatabase from '../utils';
+
+class StudentsController {
+  static async getAllStudents(req, res) {
+    res.write('This is the list of our students\n');
+    readDatabase(process.argv[2])
+      .then((students) => {
+        for (const [field, names] of Object.entries(students).sort(
+          (a, b) => a[0].localeCompare(b[0]),
+        )) {
+          res.write(
+            `Number of students in ${field}: ${
+              names.length
+            }. List: ${names.join(', ')}\n`,
+          );
+        }
+        res.end();
+      })
+      .catch(() => {
+        res.end('Cannot load the database');
+      });
+  }
+
+  static async getStudentByMajor(req, res) {
+    const { major } = req.params;
+    if (!['CS', 'SWE'].includes(major)) res.status(500).send('Major parameter must be CS or SWE');
+    readDatabase(process.argv[2])
+      .then((students) => {
+        res.write(`List: ${students[major].join(', ')}\n`);
+        res.end();
+      })
+      .catch(() => {
+        res.status(500).send('Cannot load the database');
+      });
+  }
+}
+export default StudentsController;
