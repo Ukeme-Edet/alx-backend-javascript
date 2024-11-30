3e150b680a63139de59930344a4197277bcfa689
Ukeme Edet <ukemeedet2207@gmail.com>
2024-11-30 15:46:55 +0100

    Add utility function to read and parse student database from file

---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,23 @@
+import fs from 'fs/promises';
+
+const readDatabase = async (path) => fs
+  .readFile(path, 'utf-8')
+  .then((data) => {
+    const students = data
+      .trim()
+      .split('\n')
+      .slice(1)
+      .map((line) => line.split(','))
+      .filter((student) => student.length === 4);
+    const fields = students.reduce((acc, student) => {
+      const field = student[3];
+      if (!acc[field]) acc[field] = [];
+      acc[field].push(student[0]);
+      return acc;
+    }, {});
+    return fields;
+  })
+  .catch(() => {
+    throw new Error('Cannot load the database');
+  });
+export default readDatabase;
