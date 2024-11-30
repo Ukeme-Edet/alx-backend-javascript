7a739d1b05d0faf5ec512b514753586f892707bd
Ukeme Edet <ukemeedet2207@gmail.com>
2024-11-30 15:11:03 +0100

    Refactor route handler to use underscore for unused request parameter

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   res.send('Hello Holberton School!');
 });
 app.listen(1245);
